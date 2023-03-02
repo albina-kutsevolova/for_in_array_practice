16c859edcff5132a6f8d2711abd4eb986506def9
albina-kutsevolova <albina_kutsevolova@mail.ru>
2023-03-02 14:31:24 +0300

    First commit

---
+++ for_in_array_practice_1.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// Найти эдементы массива, которые больше предыдущего
+
+if true {
+    var numbers = [3, 15, 6, 22, 12, 458, 8, 4, 1, 12]
+    var results = [Int]()
+    if !numbers.isEmpty {
+        for (index, number) in numbers.enumerated() {
+            if index != 0 {
+                if number > numbers[index - 1] {
+                    results.append(number)
+                }
+            }
+        }
+    } else {
+        print("Масив пустой")
+    }
+    print(results)
+}
